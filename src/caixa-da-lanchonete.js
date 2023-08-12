aa93e087fc0f32d21ace6c434985767069bb7e4c
Wilson Guilherme Spindler <wilsongspindler@gmail.com>
2023-08-11 23:19:36 -0300

    this.cardapio itens and value

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,7 +1,16 @@
 class CaixaDaLanchonete {
 
     constructor() {
-        
+        this.cardapio = {
+            "cafe": 3.00,
+            "chantily": 1.50,
+            "suco": 6.20,
+            "sanduiche": 6.50,
+            "queijo": 2.00,
+            "salgado": 7.25,
+            "combo1": 9.50,
+            "combo2": 7.50,
+        };
     };
 
 }
